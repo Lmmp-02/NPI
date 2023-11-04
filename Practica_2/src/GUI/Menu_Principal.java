7a851c3053f7c06e3f0003a2dc6cc0b83e43c622
jorgelerre <92641255+jorgelerre@users.noreply.github.com>
2023-11-04 22:11:04 +0100

    Actualización carpetas

--- Practica_2/src/GUI/Menu_Principal.java
+++ Practica_2/src/GUI/Menu_Principal.java
@@ -142,6 +142,7 @@ public class Menu_Principal extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Menu_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
