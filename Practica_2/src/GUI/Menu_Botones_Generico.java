1063203db1f179b6bfca01ca274086d6a7f4ef02
Luisa <luisamariameseguerperez@gmail.com>
2023-11-24 12:21:15 +0100

    Primera aproximación menú

--- Practica_2/src/GUI/Menu_Botones_Generico.java
+++ Practica_2/src/GUI/Menu_Botones_Generico.java
@@ -73,16 +73,17 @@ public class Menu_Botones_Generico extends javax.swing.JFrame { // Cambiar a JPa
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(380, 460));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 381, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 320, Short.MAX_VALUE)
         );
 
         pack();
