c1ca793486e92581f7edee3eb21ce0373b34d0e7
JMMelcrack <javiermm@correo.ugr.es>
2023-11-25 13:31:23 +0100

    Añadido script para generar todas las entradas de la tabla Menus

---
+++ Practica_2/src/scriptSQL/Menus.sql
@@ -0,0 +1,147 @@
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Lunes', '1', 'Pizza jamón cocido y queso', 
+'Lomo relleno', 'Calabacín frito', 'Mandarinas');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Lunes', '2', 'Pizza 4 quesos', 
+'Tortilla de espinacas', 'Gratinado de calabaza y patatas', 'Mandarinas');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Martes', '1', 'Rancho canario', 
+'Fritura de pescado', 'Ensalada', 'Manzana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Martes', '2', 'Rancho canario (OV)', 
+'Rollitos de primavera', 'Ensalada', 'Manzana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Miercoles', '1', 'Terrina de carne', 
+'Pollo arlesiana', 'Sopa normanda', 'Mandarinas');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Miercoles', '2', 'Pimientos rellenos de arroz y berenjena (OV)', 
+'Sopa minestrone', 'Huevos escalfados argelina', 'Mandarinas');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Jueves', '1', 'Espaguetis a la crema con bacalao', 
+'Goulash húngaro', '-', 'Piña');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Jueves', '2', 'Espaguetis agridulce (VG)', 
+'Fricandó de alcachofas (VG)', '-', 'Piña');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Viernes', '1', 'Pastel de espinacas, piñones y mozzarella', 
+'Ternera a la cazadora', 'Pimientos fritos (VG)', 'Manzana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Viernes', '2', 'Crema de calabacín', 
+'Pastel de espinacas, piñones y mozzarella (OV)', 'Verdura agridulce con cuscús de tomate seco (OV)', 'Manzana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Sabado', '1', 'Lasaña', 
+'Dorada a la espalda', 'Ensalada de verduras (OV)', 'Banana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Sabado', '2', 'Lasaña de verduras (OV)', 
+'Tempe empanado', 'Ensalada de verduras (OV)', 'Banana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Lunes', 'Almuerzo', 'Olla gitana', 
+'Muslo de pollo en escabeche de naranja', 'Judías, zanahorias y patatas', 'Manzana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Martes', 'Almuerzo', 'Macarrones a la calabresa', 
+'Surtidos de matanza con fritada', 'Habas en colorado', 'Naranja');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Miercoles', 'Almuerzo', 'Estofado de lentejas', 
+'Pechuga de pollo Kiev', 'Patatas, zanahorias y champiñones gratinados', 'Banana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Jueves', 'Almuerzo', 'Estofado de carne de cerdo', 
+'Crepineta de jamón cocido y queso', 'Pisto de membrillo', 'Piña');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Viernes', 'Almuerzo', 'Arroz cuatro delicias', 
+'Carnes salteadas a la Normanda', 'Tartiflette tradicional de Saboya', 'Pera');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Lunes', 'Cena', 'Sopa minestrone piamontesa', 
+'Cazón en amarillo', 'Ensalada de pasta', 'Flan de huevo');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Martes', 'Cena', 'Crema de verduras', 
+'Aguja a la parrilla', 'Ensaladilla rusa', 'Manzana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Miercoles', 'Cena', 'Pastel cordobés', 
+'Merluza a la vasca', 'Crema de ave reina', 'Manzana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Jueves', 'Cena', 'Sopa juliana', 
+'Paleta de cerdo a la jardinera', '-', 'Tiramisú');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Viernes', 'Cena', 'Sopa fidelina', 
+'Bacalao con berenjenas y confitura de tomate', 'Espárragos con mahonesa', 'Tarta de San Marcos');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Lunes', 'Ovolactovegetariano', 'Olla gitana', 
+'Croquetas de setas y queso', 'Judías, zanahorias y patatas', 'Mandarinas');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Martes', 'Ovolactovegetariano', 'Macarrones a la calabresa', 
+'Espaguetis de calabacín con setas cultivadas y curry', 'Habas en colorado', 'Naranja');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Miercoles', 'Ovolactovegetariano', 'Estofado de lentejas', 
+'Pastel de queso con pasas y nueces', 'Patatas, zanahorias y champiñones gratinados', 'Banana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Jueves', 'Ovolactovegetariano', 'Patatas en ajo pollo - Vegetariano', 
+'Ensalada de verduras, mango y pepino', 'Pisto de membrillo', 'Piña');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Viernes', 'Ovolactovegetariano', 'Arroz cuatro delicias', 
+'Pastel de queso con pasas y nueces', 'Espárragos con mahonesa', 'Pera');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Lunes', 'Vegano', 'Olla gitana', 
+'Salchichas veganas con salsa de tomate', 'Judías, zanahorias y patatas', 'Mandarinas');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Martes', 'Vegano', 'Macarrones a la calabresa', 
+'Espaguetis de calabacín con setas cultivadas y curry', 'Habas en colorado', 'Naranja');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Miercoles', 'Vegano', 'Estofado de lentejas', 
+'Falafel con salsa de sésamo', 'Patatas salteadas con pimientos fritos', 'Banana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Jueves', 'Vegano', 'Patatas en ajo pollo - Vegetariano', 
+'Ensalada de verduras, mango y pepino', 'Pisto de membrillo', 'Piña');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Viernes', 'Vegano', 'Arroz cuatro delicias', 
+'Migas de coliflor con setas y mojo de perejil', '-', 'Pera');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Lunes', 'T-celiaco', 'Paella marinera', 
+'Ternera en salsa de pimienta', 'Coliflor con vinagreta agridulce', 'Plátano');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Martes', 'T-celiaco', 'Estofado de lentejas', 
+'Lomo relleno', 'Gratinado de calabaza y patatas', 'Banana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Miercoles', 'T-celiaco', 'Rancho canario', 
+'Aguja a la parrilla', 'Ensalada', 'Manzana');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Jueves', 'T-celiaco', 'Terrina de carne', 
+'Pollo arlesiana', 'Sopa normanda', 'Mandarinas');
+
+INSERT INTO Menus (Dia, TipoMenu, Primero, Segundo, Acompanamiento, Postre) VALUES 
+('Viernes', 'T-celiaco', 'Macarrones a la crema con bacalao', 
+'Goulash húngaro', '-', 'Piña');
