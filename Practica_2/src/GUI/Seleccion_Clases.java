9b3b46b373bca0f32ce02f3dc3894d42e3bde8f0
Luisa <75673151+Lmmp-02@users.noreply.github.com>
2023-11-30 09:47:58 +0100

    Update Seleccion_Clases.java

--- Practica_2/src/GUI/Seleccion_Clases.java
+++ Practica_2/src/GUI/Seleccion_Clases.java
@@ -13,6 +13,7 @@ import javax.swing.DefaultComboBoxModel;
 public class Seleccion_Clases extends javax.swing.JPanel {
 
     private Ventana padre;
+	int a = 0;
     
     public Seleccion_Clases(Ventana p) {
         initComponents();
