23598cb4f9acc05f2967cd38e8e8657cb109b8e8
jorgelerre <92641255+jorgelerre@users.noreply.github.com>
2024-01-18 17:10:14 +0100

    Muestra ventana chat básica

--- Practica_3/QRGen/app/src/main/java/com/example/qrgen/InterfazOral.java
+++ Practica_3/QRGen/app/src/main/java/com/example/qrgen/InterfazOral.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.applozic.mobicomkit.api.account.register.RegistrationResponse;
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,8 +15,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.kommunicate.KmConversationBuilder;
+import io.kommunicate.KmConversationHelper;
+import io.kommunicate.KmException;
 import io.kommunicate.Kommunicate;
 import io.kommunicate.callbacks.KMLoginHandler;
+import io.kommunicate.callbacks.KmCallback;
 import io.kommunicate.users.KMUser;
 
 public class InterfazOral extends AppCompatActivity {
@@ -53,22 +58,52 @@ public class InterfazOral extends AppCompatActivity {
             metadata.put("Department" , "Mobility"); // This is an example of updating metadata.
             metadata.put("Designation" , "Software Engineer II"); // This is an example of updating metadata.
 
-            kmUser.setMetadata(metadata);
+            kmuser.setMetadata(metadata);
 
-            //Creamos el chat
+            //Inicio de sesión del usuario
             Kommunicate.login(this, kmuser, new KMLoginHandler() {
                 @Override
                 public void onSuccess(RegistrationResponse registrationResponse, Context context) {
-                    // You can perform operations such as opening the conversation, creating a new conversation or update user details on success
-                    // A completar
+                    //Creamos la conversación
+                    new KmConversationBuilder(InterfazOral.this)
+                            // Pass false if you would like to create new conversation every time user starts
+                            // a conversation. This is true by default which means only one conversation will
+                            // open for the user every time the user starts a conversation.
+                            .setSingleConversation(false)
+                            .createConversation(new KmCallback() {
+                                @Override
+                                public void onSuccess(Object message) {
+                                    Integer conversationId = (Integer) message;
+                                    //Abrimos la conversacion
+                                    try {
+                                        KmConversationHelper.openConversation(InterfazOral.this,
+                                                true,
+                                                conversationId, null);
+                                    } catch (KmException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                    //Cierra la pantalla de carga cuando termina la conversación, volviendo al menú
+                                    finish();
+                                }
+
+                                @Override
+                                public void onFailure(Object error) {
+                                    Log.d("ConversationTest", "Error en creacion conversacion KM: " + error);
+                                }
+                            });
+
                 }
 
                 @Override
                 public void onFailure(RegistrationResponse registrationResponse, Exception exception) {
                     // You can perform actions such as repeating the login call or throw an error message on failure
-                    // A completar
+                    Log.d("Error en login KM", "Error : " + exception);
                 }
             });
+
+
+
+
         }
         //Si el usuario no esta registrado, hacemos un chat como visitante
         else {
