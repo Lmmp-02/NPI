08fc860d595cab38cb29b859703839d23acbc6ca
JMMelcrack <javiermm@correo.ugr.es>
2024-01-19 01:30:28 +0100

    Escaner de QR semifuncional, no escribe en el label lo que lee

--- Practica_3/QRGen/app/src/main/java/com/example/qrgen/FragmentoQR.java
+++ Practica_3/QRGen/app/src/main/java/com/example/qrgen/FragmentoQR.java
@@ -1,5 +1,6 @@
 package com.example.qrgen;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,76 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link FragmentoQR#newInstance} factory method to
  * create an instance of this fragment.
  */
+
+// Tutorial de apoyo
+// https://www.youtube.com/watch?v=elZP3JFFdVk&ab_channel=C%C3%B3digosdeProgramaci%C3%B3n-MR
+
 public class FragmentoQR extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button btnEscanerQR;
+    EditText txtResultado;
 
     public FragmentoQR() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentoQR.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentoQR newInstance(String param1, String param2) {
+    public static FragmentoQR newInstance() {
         FragmentoQR fragment = new FragmentoQR();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qr, container, false);
+        View view = inflater.inflate(R.layout.fragment_qr, container, false);
+
+        btnEscanerQR = view.findViewById(R.id.btnEscanerQR);
+        txtResultado = view.findViewById(R.id.txtResultado);
+
+        btnEscanerQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrador = new IntentIntegrator(FragmentoQR.this.getActivity());
+                integrador.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrador.setPrompt("Lector - CDP");
+                integrador.setCameraId(0); //0 -> Cámara trasera
+                integrador.setBeepEnabled(true);
+                integrador.setBarcodeImageEnabled(true);
+                integrador.initiateScan();
+            }
+        });
+
+        return view;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if(result != null){ //Se lee algo o se cancela la lectura
+            if(result.getContents() == null) Toast.makeText(this.getActivity(), "Escaneo cancelado", Toast.LENGTH_LONG).show();
+            else{
+                Toast.makeText(this.getActivity(), result.getContents(), Toast.LENGTH_LONG).show();
+                txtResultado.setText(result.getContents());
+            }
+        }
+        else super.onActivityResult(requestCode, resultCode, data);
     }
 }
