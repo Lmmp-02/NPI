2e7bb31964b98aad42cdc5a25ef317037b0a5bfb
jorgelerre <92641255+jorgelerre@users.noreply.github.com>
2024-01-21 13:41:28 +0100

    TTS + STT + Message Listener + Fix Espera Permisos

--- Practica_3/QRGen/app/src/main/java/com/example/qrgen/InterfazOral.java
+++ Practica_3/QRGen/app/src/main/java/com/example/qrgen/InterfazOral.java
@@ -1,12 +1,23 @@
 package com.example.qrgen;
 
+import android.Manifest;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.applozic.mobicomkit.api.account.register.RegistrationResponse;
+import com.applozic.mobicomkit.api.conversation.Message;
+import com.applozic.mobicomkit.broadcast.AlEventManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -14,6 +25,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import io.kommunicate.KmConversationBuilder;
 import io.kommunicate.KmConversationHelper;
@@ -21,9 +34,12 @@ import io.kommunicate.KmException;
 import io.kommunicate.Kommunicate;
 import io.kommunicate.callbacks.KMLoginHandler;
 import io.kommunicate.callbacks.KmCallback;
+import io.kommunicate.callbacks.KmPluginEventListener;
 import io.kommunicate.users.KMUser;
+import com.applozic.mobicomkit.uiwidgets.kommunicate.KmPrefSettings;
 
-public class InterfazOral extends AppCompatActivity {
+public class InterfazOral extends AppCompatActivity implements KmPluginEventListener {
+    public static final Integer RecordAudioRequestCode = 1;
     FirebaseAuth auth;
     FirebaseDatabase database;
     DatabaseReference ref_usuario;
@@ -36,18 +52,52 @@ public class InterfazOral extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
 
+        //Comprobamos permisos. Si no los tiene, los pedimos
+        if(ContextCompat.checkSelfPermission(InterfazOral.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
+            }
+        }
+        //Si tiene los permisos, pasamos a la creacion del chat
+        else{
+            creaInterfazOral();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RecordAudioRequestCode && grantResults.length > 0 ){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this,"Permiso concedido.", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(this,"Microfono apagado. Solo disponible chat de texto.", Toast.LENGTH_LONG).show();
+        }
+        creaInterfazOral();
+    }
+
+    public void creaInterfazOral(){
         //Inicializamos Kommunicate
         Kommunicate.init(InterfazOral.this, "1046f12744c6017fb944066df8190daeb");
 
         //Obtenemos los datos del usuario actual
         FirebaseUser user = auth.getCurrentUser();
 
+        //Habilitamos TTS y STT
+        KmPrefSettings.getInstance(InterfazOral.this)
+                .enableSpeechToText(true) // Habilita el microfono para escribir mensajes
+                .enableTextToSpeech(true) // Al recibir mensajes, se leen en voz alta
+                //.setSendMessageOnSpeechEnd(true) //Permite que se envie el mensaje automaticamente al terminar de hablar
+                .setSpeechToTextLanguage("es-ES") // indicamos idioma de STT
+                .setTextToSpeechLanguage("es-ES"); // indicamos idioma de TTS
+
+
         //Si el usuario esta registrado
         if(user != null){
             //Obtenemos info del usuario
             String uid = auth.getUid();
             ref_usuario = database.getReference("user").child(uid);
-
 
             //Creamos usuario en Kommunicate
             KMUser kmuser = new KMUser();
@@ -81,6 +131,8 @@ public class InterfazOral extends AppCompatActivity {
                                     //Obtenemos el id de la conversacion
                                     Integer conversationId = (Integer) message;
 
+                                    //Registramos eventos
+                                    AlEventManager.getInstance().registerPluginEventListener(InterfazOral.this);
                                     //Mostramos info de la conversacion
                                     // Iterar sobre el Map utilizando un bucle for-each
                                     /*
@@ -129,6 +181,120 @@ public class InterfazOral extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    //Métodos que implementan la interfaz de escucha
+
+    //Comprobamos si el mensaje da una orden
+    @Override
+    public void onMessageReceived(Message message) {
+        String msg = message.getMessage();
+
+        // Definir la expresión regular con grupos de captura para $origen y $destino
+        String regex = "(\\S+)\\s*-->\\s*(\\S+)";
+
+        // Compilar la expresión regular
+        Pattern pattern = Pattern.compile(regex);
+
+        // Crear un objeto Matcher
+        Matcher matcher = pattern.matcher(msg);
+
+        // Verificar si se encuentra la coincidencia
+        if (matcher.find()) {
+            // Obtener los valores de $origen y $destino
+            String origen = matcher.group(1);
+            String destino = matcher.group(2);
+
+            // Imprimir los valores
+            System.out.println("Origen: " + origen);
+            System.out.println("Destino: " + destino);
+
+            Toast.makeText(this,"Origen: "+origen+"\tDestino: "+destino, Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this,"No encontrada accion especial", Toast.LENGTH_SHORT).show();
+        }
 
     }
+    //Metodos que implementan la interfaz de escucha, pero que no se implementan
+    @Override
+    public void onPluginLaunch() {
+
+    }
+
+    @Override
+    public void onPluginDismiss() {
+
+    }
+
+    @Override
+    public void onConversationResolved(Integer conversationId) {
+
+    }
+
+    @Override
+    public void onConversationRestarted(Integer conversationId) {
+
+    }
+
+    @Override
+    public void onRichMessageButtonClick(Integer conversationId, String actionType, Object action) {
+
+    }
+
+    @Override
+    public void onStartNewConversation(Integer conversationId) {
+
+    }
+
+    @Override
+    public void onSubmitRatingClick(Integer conversationId, Integer rating, String feedback) {
+
+    }
+
+    @Override
+    public void onMessageSent(Message message) {
+
+    }
+    @Override
+    public void onBackButtonClicked(boolean isConversationOpened) {
+
+    }
+
+    @Override
+    public void onAttachmentClick(String attachmentType) {
+
+    }
+
+    @Override
+    public void onFaqClick(String FaqUrl) {
+
+    }
+
+    @Override
+    public void onLocationClick() {
+
+    }
+
+    @Override
+    public void onNotificationClick(Message message) {
+
+    }
+
+    @Override
+    public void onVoiceButtonClick(String action) {
+
+    }
+
+    @Override
+    public void onRatingEmoticonsClick(Integer ratingValue) {
+
+    }
+
+    @Override
+    public void onRateConversationClick() {
+
+    }
+
+
 }
