cceac6c69bbc303929085789af71d0917d273c07
JMMelcrack <javiermm@correo.ugr.es>
2024-01-21 14:51:47 +0100

    Initial commit escanerNFC

--- Practica_3/QRGen/app/src/main/java/com/example/qrgen/FragmentoNFC.java
+++ Practica_3/QRGen/app/src/main/java/com/example/qrgen/FragmentoNFC.java
@@ -1,12 +1,38 @@
 package com.example.qrgen;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NfcA;
+import android.nfc.tech.NfcF;
+import android.nfc.tech.NfcV;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +41,38 @@ import android.view.ViewGroup;
  */
 public class FragmentoNFC extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button btnEscanerNFC;
 
     public FragmentoNFC() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentoNFC.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentoNFC newInstance(String param1, String param2) {
+    public static FragmentoNFC newInstance() {
         FragmentoNFC fragment = new FragmentoNFC();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nfc, container, false);
+        View view = inflater.inflate(R.layout.fragment_nfc, container, false);
+
+        btnEscanerNFC = view.findViewById(R.id.btnEscanerNFC);
+
+        btnEscanerNFC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(requireContext(), LectorNFC.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
