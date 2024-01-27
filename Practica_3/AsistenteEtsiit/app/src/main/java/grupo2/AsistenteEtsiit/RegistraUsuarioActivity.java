5784e2bad56ecb21b02e02347911291f45ec2971
JMMelcrack <javiermm@correo.ugr.es>
2024-01-27 13:52:32 +0100

    Multimenu creado correctamente

--- Practica_3/AsistenteEtsiit/app/src/main/java/grupo2/AsistenteEtsiit/RegistraUsuarioActivity.java
+++ Practica_3/AsistenteEtsiit/app/src/main/java/grupo2/AsistenteEtsiit/RegistraUsuarioActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -23,11 +25,16 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegistraUsuarioActivity extends AppCompatActivity {
-    TextView boton_login, boton_invitado;
-    EditText usuario, correo, password, re_password, grupo;
-    Spinner grado;
+    TextView boton_login, boton_invitado, txtGrado, txtCurso;
+    EditText usuario, correo, password, re_password;
+    Spinner grado, curso;
     Button boton_registro;
+
+    List<String> opcionesCurso;
     FirebaseAuth auth;
     String emailRegex = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
     FirebaseDatabase database;
@@ -46,9 +53,60 @@ public class RegistraUsuarioActivity extends AppCompatActivity {
         password = findViewById(R.id.editTextPassword);
         re_password = findViewById(R.id.editTextPassword2);
         grado = findViewById(R.id.spinnerGrado);
-        grupo = findViewById(R.id.editTextCurso);
+        curso = findViewById(R.id.spinnerCurso);
         boton_login = findViewById(R.id.textoRegistro);
         boton_registro = findViewById(R.id.boton_entrar);
+        txtGrado = findViewById(R.id.txtGrado);
+        txtCurso = findViewById(R.id.txtCurso);
+
+        // Define las opciones disponibles en el Spinner
+        String[] opcionesGrado = {"Ingeniería Informática", "Ingeniería de Telecomunicaciones"};
+
+        // Crea un ArrayAdapter usando las opciones y un layout predeterminado para los elementos del Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcionesGrado);
+
+        // Especifica el layout a utilizar cuando aparece la lista de opciones
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Asigna el adaptador al Spinner
+        grado.setAdapter(adapter);
+
+        // Opciones iniciales para el segundo Spinner
+        opcionesCurso = new ArrayList<>();
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcionesCurso);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        curso.setAdapter(adapter2);
+
+        // Maneja el evento de selección del Spinner
+        grado.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selectedOption = opcionesGrado[position];
+                txtGrado.setText(selectedOption);
+
+                // Actualiza las opciones del segundo Spinner según la selección en el primer Spinner
+                actualizarCursos(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Maneja el caso en el que no se selecciona nada (opcional)
+            }
+        });
+
+        // Evento Spinner 2
+        curso.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selectedOption = opcionesCurso.get(position);
+                txtCurso.setText(selectedOption);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Maneja el caso en el que no se selecciona nada (opcional)
+            }
+        });
 
 
         //Si se pulsa iniciar sesion, vamos a dicha actividad
@@ -71,8 +129,8 @@ public class RegistraUsuarioActivity extends AppCompatActivity {
                 String pass = password.getText().toString();
                 String pass2 = re_password.getText().toString();
                 String mail = correo.getText().toString();
-                String grad = grado.getText().toString();
-                String grup = grupo.getText().toString();
+                String grad = txtGrado.getText().toString();
+                String grup = txtCurso.getText().toString();
 
                 if(TextUtils.isEmpty(user) || TextUtils.isEmpty(mail) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(pass2)){
                     Toast.makeText(RegistraUsuarioActivity.this, "Faltan campos obligatorios por rellenar", Toast.LENGTH_SHORT).show();
@@ -140,6 +198,30 @@ public class RegistraUsuarioActivity extends AppCompatActivity {
 
     }
 
+    // Método para actualizar las opciones del segundo Spinner
+    private void actualizarCursos(int position) {
+        // Lógica para determinar las opciones del segundo Spinner según la selección en el primer Spinner
+        // Aquí puedes agregar tu lógica específica para cada posición en el primer Spinner
+
+        List<String> nuevasOpciones = new ArrayList<>();
+        if (position == 0) {
+            nuevasOpciones.add("4CSI");
+            nuevasOpciones.add("4TI");
+            nuevasOpciones.add("4IS");
+            nuevasOpciones.add("4IC");
+            nuevasOpciones.add("4SI");
+        } else{
+            nuevasOpciones.add("4A");
+            nuevasOpciones.add("4B");
+        }
+
+        opcionesCurso = nuevasOpciones;
+
+        // Actualiza las opciones del segundo Spinner
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opcionesCurso);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        curso.setAdapter(adapter2);
+    }
 }
 
 
